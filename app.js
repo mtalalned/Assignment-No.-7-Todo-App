b4d339d2cb3e0382413620375c10d663c2aaefab
mtalalned <muhammadtalalned@gmail.com>
2024-08-04 11:40:11 -0700

    Todo App Completed

--- app.js
+++ app.js
@@ -10,13 +10,19 @@ function renderTodo (){
     for (var i = 0; i < Todo.length; i++){
         list.innerHTML += `
         <div class='list-main-div d-flex justify-content-start column-gap-3'>
-        
-        <div class='p-div bg-white border rounded d-flex justify-content-start align-items-center w-75'><p class='ps-2'>${i+1}. ${Todo[i]}</p></div>
-        
-        <div class='button-div d-flex justify-content-center w-25 column-gap-2'><button onclick='deleteitem(${i})' class='delete-button border text-white px-2 pt-2 pb-2 rounded'>Delete</button>
-        
-        <button onclick='editItem(${i})' class='Edit-button border text-white px-3 pt-2 pb-2 rounded'>Edit</button></div>
-        
+            <div class='p-div bg-white rounded d-flex justify-content-start align-items-center w-75 border'>
+                <p class='ps-2 w-100'>${i+1}. ${Todo[i]}</p>
+            </div>
+
+            <div class='button-div d-flex justify-content-center align-items-center w-25 column-gap-2'>
+                <div>
+                    <button onclick='deleteitem(${i})' class='delete-button border text-white px-2 pt-2 pb-2 rounded'>delete</button>
+                </div>
+                
+                <div>
+                    <button onclick='editItem(${i})' class='edit-button border text-white px-3 pt-2 pb-2 rounded'>edit</button>
+                </div>
+            </div>
         </div>`
     }
 }
