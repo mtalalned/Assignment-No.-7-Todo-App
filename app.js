89239d62b412b9fe920f9fdafcb78d85b18c97e5
mtalalned <muhammadtalalned@gmail.com>
2024-08-05 08:18:08 -0700

    input change is started

--- app.js
+++ app.js
@@ -46,9 +46,39 @@ function deleteitem (index){
 
 function editItem (index) {
     
-    var inputItem = prompt ('Enter Item')
+    
+    for (var i = 0; i < Todo.length; i++){
+        
+        if (i = index) {
+            list.innerHTML += `
+            <div class='list-main-div d-flex justify-content-start column-gap-3'>
+                <div class='p-div bg-white rounded d-flex justify-content-start align-items-center w-75 border'>
+                    <input type="text" placeholder="Enter Item" class="text-start ps-2 w-75 border rounded" id="add-input">
+                </div>
 
-    Todo.splice (index , 1 , inputItem)
+                <div class='button-div d-flex justify-content-center align-items-center w-25 column-gap-2'>
+                    <button onclick="addeditTodo(${i})" class="w-25 border text-white bg-gradient pt-2 pb-2 fs-5 add-todo-button rounded">Add Todo</button>
+                </div>
+            </div>`
+        continue
+        }
+    
+            list.innerHTML += `
+            <div class='list-main-div d-flex justify-content-start column-gap-3'>
+                <div class='p-div bg-white rounded d-flex justify-content-start align-items-center w-75 border'>
+                    <p class='ps-2 w-100'>${i+1}. ${Todo[i]}</p>
+                </div>
 
-    renderTodo()
+                <div class='button-div d-flex justify-content-center align-items-center w-25 column-gap-2'>
+                    <div>
+                        <button onclick='deleteitem(${i})' class='delete-button border text-white px-2 pt-2 pb-2 rounded'>delete</button>
+                    </div>
+                
+                    <div>
+                        <button onclick='editItem(${i})' class='edit-button border text-white px-3 pt-2 pb-2 rounded'>edit</button>
+                    </div>
+                </div>
+            </div>`
+        }
+
 }
