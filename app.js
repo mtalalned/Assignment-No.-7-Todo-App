ca2e99852d50df6113824696ad9b1fb833a40a75
mtalalned <muhammadtalalned@gmail.com>
2024-08-05 08:50:49 -0700

    Todo App Completed

--- app.js
+++ app.js
@@ -3,6 +3,44 @@ var Todo = []
 var list = document.querySelector ('#item-div')
 
 
+// edit todo render function
+function rendereditTodo(index){
+    
+    list.innerHTML =''
+
+    for (var i = 0; i < Todo.length; i++){
+
+        if (i === index) {
+            
+            list.innerHTML += `
+            <div class='list-main-div d-flex justify-content-start column-gap-3'>
+                <input type="text" placeholder="Enter Item" class="text-start ps-2 w-75 border rounded" id="add-input">
+                <button onclick="addeditTodo(${i})" class="w-25 border text-white bg-gradient pt-2 pb-2 replace-todo-button rounded">Replace Todo</button>
+            </div>`
+            continue
+        }
+    
+            list.innerHTML += `
+            <div class='list-main-div d-flex justify-content-start column-gap-3'>
+                <div class='p-div bg-white rounded d-flex justify-content-start align-items-center w-75 border'>
+                    <p class='ps-2 w-100'>${i+1}. ${Todo[i]}</p>
+                </div>
+
+                <div class='button-div d-flex justify-content-center align-items-center w-25 column-gap-2'>
+                    <div>
+                        <button onclick='deleteitem(${i})' class='delete-button border text-white px-2 pt-2 pb-2 rounded'>delete</button>
+                    </div>
+                
+                    <div>
+                        <button onclick='editItem(${i})' class='edit-button border text-white px-3 pt-2 pb-2 rounded'>edit</button>
+                    </div>
+                </div>
+            </div>`
+        }
+}
+
+
+// Render todo
 function renderTodo (){
     
     list.innerHTML = ''
@@ -27,7 +65,7 @@ function renderTodo (){
     }
 }
 
-
+// function add todo
 function addTodo () {
     
     Todo.push (input.value)
@@ -37,6 +75,8 @@ function addTodo () {
     input.value =''
 }
 
+
+// function delete item
 function deleteitem (index){
     
     Todo.splice (index, 1)
@@ -44,41 +84,20 @@ function deleteitem (index){
     renderTodo()
 }
 
+// function edit item
 function editItem (index) {
     
+    rendereditTodo(index)
     
-    for (var i = 0; i < Todo.length; i++){
-        
-        if (i = index) {
-            list.innerHTML += `
-            <div class='list-main-div d-flex justify-content-start column-gap-3'>
-                <div class='p-div bg-white rounded d-flex justify-content-start align-items-center w-75 border'>
-                    <input type="text" placeholder="Enter Item" class="text-start ps-2 w-75 border rounded" id="add-input">
-                </div>
+}
 
-                <div class='button-div d-flex justify-content-center align-items-center w-25 column-gap-2'>
-                    <button onclick="addeditTodo(${i})" class="w-25 border text-white bg-gradient pt-2 pb-2 fs-5 add-todo-button rounded">Add Todo</button>
-                </div>
-            </div>`
-        continue
-        }
+// function add edit todo
+function addeditTodo(index){
     
-            list.innerHTML += `
-            <div class='list-main-div d-flex justify-content-start column-gap-3'>
-                <div class='p-div bg-white rounded d-flex justify-content-start align-items-center w-75 border'>
-                    <p class='ps-2 w-100'>${i+1}. ${Todo[i]}</p>
-                </div>
+    var replacedValue = document.querySelector ('#add-input')
+    
+    Todo.splice (index, 1, replacedValue.value)
 
-                <div class='button-div d-flex justify-content-center align-items-center w-25 column-gap-2'>
-                    <div>
-                        <button onclick='deleteitem(${i})' class='delete-button border text-white px-2 pt-2 pb-2 rounded'>delete</button>
-                    </div>
-                
-                    <div>
-                        <button onclick='editItem(${i})' class='edit-button border text-white px-3 pt-2 pb-2 rounded'>edit</button>
-                    </div>
-                </div>
-            </div>`
-        }
+    renderTodo()
 
 }
